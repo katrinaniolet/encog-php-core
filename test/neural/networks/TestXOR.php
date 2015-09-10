df854627afa4663233ba5a6b07c7d3fed554c926
Katrina Niolet <katrina@kf5utn.net>
2015-09-10 15:23:17 -0500

    Initial port of the remaining XOR Test support functions

--- test/neural/networks/TestXOR.php
+++ test/neural/networks/TestXOR.php
@@ -35,16 +35,21 @@ const XOR_IDEAL = [[ 0.0 ], [ 1.0 ], [ 1.0 ], [ 0.0 ]];
 
 const XOR_IDEAL2 = [[ 1.0, 0.0 ], [ 0.0,1.0 ], [ 1.0,0.0 ], [ 0.0,1.0 ]];
 
-/*TODO(katrina) public static boolean verifyXOR(MLRegression network,double tolerance)
+/**
+ * @param MLRegression network
+ * @param double tolerance
+ * @return bool
+ */
+function verifyXOR(MLRegression $network,$tolerance)
 {
-	for(int trainingSet=0;trainingSet<XOR.XOR_IDEAL.length;trainingSet++)
+	for($trainingSet=0;$trainingSet<count(XOR_IDEAL);++$trainingSet)
 	{
-		MLData actual = network.compute(new BasicMLData(XOR.XOR_INPUT[trainingSet]));
+		$actual = $network->compute(new BasicMLData(XOR_INPUT[$trainingSet]));
 
-		for(int i=0;i<XOR.XOR_IDEAL[0].length;i++)
+		for($i=0;$i<count(XOR_IDEAL[0]);++$i)
 		{
-			double diff = Math.abs(actual.getData(i)-XOR.XOR_IDEAL[trainingSet][i]);
-			if( diff>tolerance )
+			$diff = \abs($actual->getData($i)-XOR_IDEAL[$trainingSet][$i]);
+			if( $diff>$tolerance )
 				return false;
 		}
 
@@ -53,75 +58,78 @@ const XOR_IDEAL2 = [[ 1.0, 0.0 ], [ 0.0,1.0 ], [ 1.0,0.0 ], [ 0.0,1.0 ]];
 	return true;
 }
 
-public static MLDataSet createXORDataSet()
+/**
+ * @return MLDataSet
+ */
+function createXORDataSet()
 {
-	return new BasicMLDataSet(XOR.XOR_INPUT,XOR.XOR_IDEAL);
+	return new BasicMLDataSet(XOR_INPUT,XOR_IDEAL);
 }
 
-public static void testXORDataSet(MLDataSet set)
+function testXORDataSet(MLDataSet $set)
 {
-	int row = 0;
-	for(MLDataPair item: set)
+	$row = 0;
+	foreach($set as $item)
 	{
-		for(int i=0;i<XOR.XOR_INPUT[0].length;i++)
+		for($i=0;$i<count(XOR_INPUT[0]);++$i)
 		{
-			TestCase.assertEquals(item.getInput().getData(i),
-					XOR.XOR_INPUT[row][i]);
+			$this->assertEquals($item->getInput()->getData($i),
+					XOR_INPUT[$row][$i]);
 		}
 
-		for(int i=0;i<XOR.XOR_IDEAL[0].length;i++)
+		for($i=0;$i<count(XOR_IDEAL[0]);++$i)
 		{
-			TestCase.assertEquals(item.getIdeal().getData(i),
-					XOR.XOR_IDEAL[row][i]);
+			$this->assertEquals($item->getIdeal()->getData($i),
+					XOR_IDEAL[$row][$i]);
 		}
 
-		row++;
+		++$row;
 	}
 }
 
-public static BasicNetwork createTrainedXOR()
+function createTrainedXOR()
 {
-	double[] TRAINED_XOR_WEIGHTS = { 25.427193285452972,-26.92000502099534,20.76598054603445,-12.921266548020219,-0.9223427050161919,-1.0588373209475093,-3.80109620509867,3.1764938777876837,80.98981535707951,-75.5552829139118,37.089976176012634,74.85166823997326,75.20561368661059,-37.18307123471437,-21.044949631177417,43.81815044327334,9.648991753485689 };
-	BasicNetwork network = EncogUtility.simpleFeedForward(2, 4, 0, 1, false);
-	NetworkCODEC.arrayToNetwork(TRAINED_XOR_WEIGHTS, network);
-	return network;
+	$TRAINED_XOR_WEIGHTS = [ 25.427193285452972,-26.92000502099534,20.76598054603445,-12.921266548020219,-0.9223427050161919,-1.0588373209475093,-3.80109620509867,3.1764938777876837,80.98981535707951,-75.5552829139118,37.089976176012634,74.85166823997326,75.20561368661059,-37.18307123471437,-21.044949631177417,43.81815044327334,9.648991753485689 ];
+	$network = EncogUtility\simpleFeedForward(2, 4, 0, 1, false);
+	NetworkCODEC\arrayToNetwork($TRAINED_XOR_WEIGHTS, $network);
+	return $network;
 }
 
-public static BasicNetwork createUnTrainedXOR()
+function createUnTrainedXOR()
 {
-	double[] TRAINED_XOR_WEIGHTS = { -0.427193285452972,0.92000502099534,-0.76598054603445,-0.921266548020219,-0.9223427050161919,-0.0588373209475093,-0.80109620509867,3.1764938777876837,0.98981535707951,-0.5552829139118,0.089976176012634,0.85166823997326,0.20561368661059,0.18307123471437,0.044949631177417,0.81815044327334,0.648991753485689 };
-	BasicNetwork network = EncogUtility.simpleFeedForward(2, 4, 0, 1, false);
-	NetworkCODEC.arrayToNetwork(TRAINED_XOR_WEIGHTS, network);
-	return network;
+	$TRAINED_XOR_WEIGHTS = [ -0.427193285452972,0.92000502099534,-0.76598054603445,-0.921266548020219,-0.9223427050161919,-0.0588373209475093,-0.80109620509867,3.1764938777876837,0.98981535707951,-0.5552829139118,0.089976176012634,0.85166823997326,0.20561368661059,0.18307123471437,0.044949631177417,0.81815044327334,0.648991753485689 ];
+	$network = EncogUtility\simpleFeedForward(2, 4, 0, 1, false);
+	$NetworkCODEC\arrayToNetwork($TRAINED_XOR_WEIGHTS, $network);
+	return $network;
 }
 
-public static BasicNetwork createThreeLayerNet()
+function createThreeLayerNet()
 {
-	BasicNetwork network = new BasicNetwork();
-	network.addLayer(new BasicLayer(2));
-	network.addLayer(new BasicLayer(3));
-	network.addLayer(new BasicLayer(1));
-	network.getStructure().finalizeStructure();
-	network.reset();
-	return network;
+	$network = new BasicNetwork();
+	$network->addLayer(new BasicLayer(2));
+	$network->addLayer(new BasicLayer(3));
+	$network->addLayer(new BasicLayer(1));
+	$network->getStructure()->finalizeStructure();
+	$network->reset();
+	return $network;
 }
 
-public static MLDataSet createNoisyXORDataSet(int count) {
-	MLDataSet result = new BasicMLDataSet();
-	for(int i=0;i<count;i++) {
-		for(int j=0;j<4;j++) {
-			MLData inputData = new BasicMLData(XOR_INPUT[j]);
-			MLData idealData = new BasicMLData(XOR_IDEAL[j]);
-			MLDataPair pair = new BasicMLDataPair(inputData,idealData);
-			inputData.setData(0, inputData.getData(0)+RangeRandomizer.randomize(-0.1, 0.1));
-			inputData.setData(1, inputData.getData(1)+RangeRandomizer.randomize(-0.1, 0.1));
-			result.add(pair);
+function createNoisyXORDataSet($count) {
+	$result = new BasicMLDataSet();
+	for($i=0;$i<$count;++$i) {
+		for($j=0;$j<4;++$j) {
+			$inputData = new BasicMLData(XOR_INPUT[$j]);
+			$idealData = new BasicMLData(XOR_IDEAL[$j]);
+			$pair = new BasicMLDataPair($inputData,$idealData);
+			$inputData->setData(0, $inputData->getData(0)+RangeRandomizer\randomize(-0.1, 0.1));
+			$inputData->setData(1, $inputData->getData(1)+RangeRandomizer\randomize(-0.1, 0.1));
+			$result->add($pair);
 		}
 	}
-	return result;
+	return $result;
 }
 
-public static FreeformNetwork createTrainedFreeformXOR() {
-	BasicNetwork network = createTrainedXOR();
-	return new FreeformNetwork(network);
-}*/
+function createTrainedFreeformXOR() {
+	$network = createTrainedXOR();
+	return new FreeformNetwork($network);
+}
